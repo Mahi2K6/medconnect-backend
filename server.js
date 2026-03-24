2c375e5c9628eeac219ddb8496ebba3dd8736550
Mahidhar <vmahidhar55@gmail.com>
2026-03-24 09:08:51 +0530

    fix(cors): update CORS policy to allow Mobile APK to send POST requests

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const path = require("path");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
